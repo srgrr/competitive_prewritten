2536deee846009e2dcfe76f46487f943263c2ff2
raccoon <sergio.r.guasch@gmail.com>
2026-08-21 18:14:25 +0200

    add template

---
+++ template.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <numeric>
+#include <limits>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+#define MULTI int t; cin >> t
+#define SINGLE int t = 1
+
+class Solution {
+public:
+    Solution(int tc) {
+
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    MULTI;
+    
+    for (int tc = 1; tc <= t; ++tc) {
+        Solution _(tc);
+    }
+
+    return 0;
+}
